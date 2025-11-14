2e899e80f0d8dabf428f8ab2e897c04ce3117f50
Rit1760 <ritik.kng.mehra@gmail.com>
2025-11-14 14:16:43 +0530

    update message

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,8 @@ export default function RootLayout({ children }) {
   //     document.removeEventListener("mousemove", moveCursor);
   //   };
   // }, []);
+
+  
   return (
     <html lang="en">
       <body>
